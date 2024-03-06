5e424a387d286e6519976fb66d747f38bafa5aad
Rien Heuver <rienheuver@gmail.com>
2024-03-06 16:02:54 +0100

    Add stats and window resize

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,7 @@ import {
 } from "three";
 
 import { GLTFLoader, OrbitControls } from "three/examples/jsm/Addons.js";
+import Stats from "three/examples/jsm/libs/stats.module.js";
 import "./styles.css";
 
 const scene = new Scene();
@@ -27,6 +28,7 @@ controls.enableDamping = true;
 
 const loader = new GLTFLoader();
 loader.load("./experiment1.glb", (glb) => {
+  scene.add(glb.scene);
   glb.scene.traverse((node) => {
     if (node instanceof Mesh) {
       const standardMat = node.material;
@@ -42,40 +44,25 @@ loader.load("./experiment1.glb", (glb) => {
     if (node instanceof DirectionalLight) {
       node.intensity = node.intensity / 10000;
     }
-    console.log(node);
   });
-  scene.add(glb.scene);
 });
 
 camera.position.z = 5;
 camera.position.y = 2;
 
-// Non-shadow lighting
-// scene.add(new HemisphereLight(0x87ceeb, 0xccffcc, 0.3));
+const stats = new Stats();
+document.body.appendChild(stats.dom);
+window.addEventListener("resize", () => {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
 
-// Sun
-const sun = new DirectionalLight(0xffffff, 0.1);
-sun.position.set(30, 70, 30);
-// sun.castShadow = true;
-sun.shadow.mapSize.width = 1024;
-sun.shadow.mapSize.height = 1024;
-sun.shadow.camera.near = 0.01;
-// scene.add(sun);
-
-// Point (like a lantern)
-for (let i = 0; i < 3; i++) {
-  const lantern = new PointLight(0xffffff, 20);
-  lantern.position.set(i * 10, 3, 0);
-  lantern.castShadow = true;
-  // scene.add(lantern);
-}
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
 
 function animate() {
   requestAnimationFrame(animate);
   controls.update();
-
-  // cube.rotation.x += 0.01;
-  // cube.rotation.z += 0.01;
+  stats.update();
 
   renderer.render(scene, camera);
 }
